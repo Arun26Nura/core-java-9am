ba5dc529f5c25da89abf69ab2bd8060045165182
Arun26Nura <arunkumarmohan1@gmail.com>
2023-03-14 10:18:30 +0530

    Initial commit

---
+++ com/besant/control/ForLoopDemo.java
@@ -0,0 +1,34 @@
+package com.besant.control;
+
+public class ForLoopDemo {
+    public static void main(String[] args) {
+        // Loop statements to perform repated actions.
+
+        // For -loop
+        // for ( variable initialization; condititon for n number of times; increment operators or loop increments){
+        // }
+        // 1 to 10
+
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+
+        // while(true){
+        // }
+        int i=0;
+        while(i <=10){
+            System.out.println(i);
+            i++;
+        }
+
+        int j=0;
+        do {
+            System.out.println(j);
+            j++;
+        }while (j > 0  && j<=10);
+
+
+
+
+    }
+}
---
+++ com/besant/control/SwitchExpressions.java
@@ -0,0 +1,16 @@
+package com.besant.control;
+
+public class SwitchExpressions {
+    public static void main(String[] args) {
+        int day=1;
+
+     switch (day){
+            case 1 ->{
+                System.out.println("Monday");
+            }
+            case 2 ->{
+                System.out.println("Tuesday");
+            }
+        };
+    }
+}
